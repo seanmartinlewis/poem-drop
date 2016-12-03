26f45b63d1ec1a47738bf070b1730883d4fea764
sean lewis <seanmartinlewis@gmail.com>
2016-12-03 12:13:53 -0600

    set up react router and home page, installed auth0 and axios

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+// import {Link} from 'react-router';
+
+class Home extends Component {
+  render(){
+    return(
+      <div className="home">
+        <h1>Poem</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Home.js'
 import './index.css';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ <Router history={hashHistory}>
+ <Route path='/' component={App}>
+ <IndexRoute component={Home} />
+ {/* <Route path=”resume” component={Resume}/>
+ <Route path=”contact” component={Contact}/> */}
+ </Route>
+ </Router>,
+ document.getElementById('root')
 );
